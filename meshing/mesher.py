0c67d20886f22de7716ccc12f06b91f872e016e2
fabianbachl <fabian.bachl@usound.com>
2026-03-09 15:34:52 +0100

    Initial exploration

---
+++ meshing/mesher.py
@@ -0,0 +1,13 @@
+import numpy as np
+from stl import mesh
+
+class Mesher:
+    def __init__(self):
+        pass
+
+    def create_mesh(self, heightmap, dimensions):
+        # Create vertices and faces for the mesh
+        vertices = []
+        faces = []
+
+        
